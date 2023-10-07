51d22a6461a5cb20e8913ea126698279792f87ac
George Lambrakis <yorgo12345@hotmail.com>
2023-10-07 13:45:28 -0400

    replaces the files with a Modified version of the demo. It cuts a lot of stuff out and toggles the tracker.

--- app.js
+++ app.js
@@ -1,136 +1,114 @@
+
 import { HandLandmarker, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+
+let runningMode = "VIDEO";
+let enableWebcamButton;
+let trackingButton;
+let tracking = true;
+
+
+// Before we can use HandLandmarker class we must wait for it to finish loading. Machine Learning models can be large and take a moment to get everything needed to run.
 let handLandmarker = undefined;
-let runningMode = "IMAGE";
+const createHandLandmarker = async () => {
+    const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+    handLandmarker = await HandLandmarker.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+            delegate: "GPU"
+        },
+        runningMode: runningMode,
+        numHands: 2
+    });
+
+};
+createHandLandmarker();
 
 
-// Accessing required HTML elements
+
+
+
+// Make the tracking button work
+trackingButton = document.getElementById("trackerButton");
+trackingButton.addEventListener("click",toggletracking)
+function toggletracking(event){ tracking = !tracking; predictWebcam(); }
+
+
+
+
+
 const video = document.getElementById("webcam");
-const enableWebcamButton = document.getElementById("webcamButton");
-const fullscreenButton = document.getElementById("fullscreenButton");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
 
-class UIManager {
-    constructor(drawEngine) {
-        this.drawEngine = drawEngine;
-        this.ctx = this.drawEngine.ctx;  // Accessing the canvas context from the DrawEngine
-    }
+enableWebcamButton = document.getElementById("webcamButton");
 
-    drawText(text, x, y) {
-        this.ctx.font = '24px Arial';
-        this.ctx.fillStyle = 'red';
-        this.ctx.fillText(text, x, y);
-    }
-
-    drawCircle(x, y, radius, color) {
-        this.ctx.beginPath();
-        this.ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-        this.ctx.fillStyle = color;
-        this.ctx.fill();
-        this.ctx.lineWidth = 2;
-        this.ctx.strokeStyle = '#003300';
-        this.ctx.stroke();
-    }
-
-    // Add other UI-specific methods here, like drawButton, displayScore, etc.
+// Check if webcam access is supported.  If not: disable the button and change it's text 
+const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
+if (hasGetUserMedia()) {
+    enableWebcamButton.addEventListener("click", enableCam);
+}
+else {
+    enableWebcamButton.removeEventListener("click", enableCam);
+    enableWebcamButton.innerText = "Camera Not Fuond";
 }
 
-class WebcamManager 
+
+
+
+
+// Enable the live webcam view and start detection.
+function enableCam(event) {
+    
+    if (!handLandmarker) {
+        console.log("Wait! objectDetector not loaded yet.");
+        return;
+    }
+
+    // Activate the webcam stream. This asks the user for permission to use the camera, if Granted the browser returns a MEDIASTREAM object
+    navigator.mediaDevices.getUserMedia( {video:true} ).then( (stream) => {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predictWebcam);
+    });
+    enableWebcamButton.removeEventListener("click", enableCam);
+    //console.log(video.srcObject);
+}
+
+
+
+
+
+let lastVideoTime = -1;
+let results = undefined;
+//console.log(video);
+async function predictWebcam() 
 {
-    constructor(videoElement) {
-        this.video = videoElement;
-    }
+    //console.log("predictiWebcam() happening!!!")
+    canvasElement.style.width = video.videoWidth;
+    canvasElement.style.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvasElement.height = video.videoHeight;
 
-    start() {
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then(stream => {
-                this.video.srcObject = stream;
-                this.video.onloadedmetadata = () => 
-                {
-                    outputCanvas.width = this.video.videoWidth;
-                    outputCanvas.height = this.video.videoHeight;
-                };
-                this.video.style.display = "block";
-                enableWebcamButton.innerText = "DISABLE WEBCAM";
-            })
-            .catch(error => {
-                console.error("Error accessing webcam:", error);
+    let startTimeMs = performance.now();
+    if (lastVideoTime !== video.currentTime) {
+        lastVideoTime = video.currentTime;
+        results = handLandmarker.detectForVideo(video, startTimeMs);
+    }
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    if (results.landmarks) {
+        for (const landmarks of results.landmarks) {
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+                color: "#00FF00",
+                lineWidth: 1
             });
-    }
-
-    stop() {
-        if (this.video.srcObject) {
-            this.video.srcObject.getTracks().forEach(track => track.stop());
-            this.video.srcObject = null;
-            this.video.style.display = "none";
-            enableWebcamButton.innerText = "ENABLE WEBCAM";
+            drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: .1 });
         }
     }
-
-    initialize() {
-        enableWebcamButton.addEventListener("click", () => {
-            if (!video.srcObject) {
-                this.start();
-            } else {
-                this.stop();
-            }
-        });
-
-        fullscreenButton.addEventListener("click", function() {
-            if (video.requestFullscreen) {
-                video.requestFullscreen();
-            } else if (video.webkitRequestFullscreen) { /* Safari */
-                video.webkitRequestFullscreen();
-            } else if (video.msRequestFullscreen) { /* IE/Edge */
-                video.msRequestFullscreen();
-            }
-        });
+    canvasCtx.restore();
+    // Call this function again to keep predicting when the browser is ready.
+    if (tracking) {
+        window.requestAnimationFrame(predictWebcam);
+    }else{
+        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     }
 }
-
-class CVProcceser {
-    
-}
-
-class DrawEngine {
-    constructor(canvasElement) {
-        this.canvas = canvasElement;
-        this.ctx = this.canvas.getContext('2d');
-    }
-
-}
-
-
-
-class GameEngine {
-    constructor(videoElement, canvasElement) {
-        // Initialize the WebcamManager and DrawEngine within the GameEngine
-        this.webcamManager = new WebcamManager(videoElement);
-        this.drawEngine = new DrawEngine(canvasElement);
-        this.uimanager = new UIManager(this.drawEngine);
-
-    }
-    
-    initialize() {
-        // Start the WebcamManager
-        this.webcamManager.initialize();
-
-    }
-
-    // A method to test the DrawEngine by drawing some text and a circle
-    testDrawEngine() 
-    {
-        // Draw Text
-        this.uimanager.drawText('Canvas Ready!', 10, 50);
-        
-        // Draw a circle with center (100,100) and radius 50
-        this.uimanager.drawCircle(100, 100, 50, 'blue');
-    }
-
-    
-}
-
-
-// Main Execution
-const outputCanvas = document.getElementById('outputCanvas');
-const gameEngine = new GameEngine(video, outputCanvas);
-gameEngine.initialize();
