8727c15bff49117f3096da31bd37a5add53dc910
momentollogy <102498438+momentollogy@users.noreply.github.com>
2023-10-07 19:12:39 -0400

    Removed Media Pipe pictures and added XYZ coordinates.

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-// Copyright 2023 The MediaPipe Authors.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//      http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
 import { HandLandmarker, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.6";
 const demosSection = document.getElementById("demos");
 let handLandmarker = undefined;
 let runningMode = "IMAGE";
 let enableWebcamButton;
 let webcamRunning = false;
-// Before we can use HandLandmarker class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
+
 const createHandLandmarker = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
     );
@@ -32,20 +21,11 @@ const createHandLandmarker = async () => {
     demosSection.classList.remove("invisible");
 };
 createHandLandmarker();
-/********************************************************************
-// Demo 1: Grab a bunch of images from the page and detection them
-// upon click.
-********************************************************************/
-// In this demo, we have put all our clickable images in divs with the
-// CSS class 'detectionOnClick'. Lets get all the elements that have
-// this class.
+
 const imageContainers = document.getElementsByClassName("detectOnClick");
-// Now let's go through all of these and add a click event listener.
 for (let i = 0; i < imageContainers.length; i++) {
-    // Add event listener to the child element whichis the img element.
     imageContainers[i].children[0].addEventListener("click", handleClick);
 }
-// When an image is clicked, let's detect it and display results!
 async function handleClick(event) {
     if (!handLandmarker) {
         console.log("Wait for handLandmarker to load before clicking!");
@@ -61,10 +41,7 @@ async function handleClick(event) {
         const n = allCanvas[i];
         n.parentNode.removeChild(n);
     }
-    // We can call handLandmarker.detect as many times as we like with
-    // different image data each time. This returns a promise
-    // which we wait to complete and then call a function to
-    // print out the results of the prediction.
+
     const handLandmarkerResult = handLandmarker.detect(event.target);
     console.log(handLandmarkerResult.handednesses[0][0]);
     const canvas = document.createElement("canvas");
@@ -90,16 +67,13 @@ async function handleClick(event) {
         drawLandmarks(cxt, landmarks, { color: "#FF0000", lineWidth: 1 });
     }
 }
-/********************************************************************
-// Demo 2: Continuously grab image from webcam stream and detect it.
-********************************************************************/
+
 const video = document.getElementById("webcam");
 const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
 // Check if webcam access is supported.
 const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
+
 if (hasGetUserMedia()) {
     enableWebcamButton = document.getElementById("webcamButton");
     enableWebcamButton.addEventListener("click", enableCam);
@@ -107,7 +81,7 @@ if (hasGetUserMedia()) {
 else {
     console.warn("getUserMedia() is not supported by your browser");
 }
-// Enable the live webcam view and start detection.
+
 function enableCam(event) {
     if (!handLandmarker) {
         console.log("Wait! objectDetector not loaded yet.");
