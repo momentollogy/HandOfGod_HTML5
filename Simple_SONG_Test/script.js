47aa61e9b65ae58a4d8ff21c7395b745422b70d9
momentollogy <102498438+momentollogy@users.noreply.github.com>
2023-10-18 00:05:47 -0400

    Song plays/stops etc.  Attempt at building moving beats. Currently json file not being read and displayed on circle.

--- Simple_SONG_Test/script.js
+++ Simple_SONG_Test/script.js
@@ -1,12 +1,75 @@
-let songStartTime = 0;
-let songElapsedTime = 0;
-let songOffset = 0;
-let recordedMoments = [];
+let currentPulseFactor = 1;
 let circleRadius = 50;
 let lastPulseTime = 0;
 let pulseDuration = 200; // duration for which the circle will stay enlarged after a pulse
 let pulseFactor = 1.5; // how much bigger the circle will become during a pulse
+let drawingRadius = circleRadius * currentPulseFactor;
+let songStartTime = 0;
+let songElapsedTime = 0;
+let songOffset = 0;
+let recordedMoments = [];
+
+
 let loadedBeats = [];
+
+class BeatManager {
+    constructor() {
+        this.beats = [];  // Active beats on the screen
+        this.spawnedBeats = 0;  // Counter to keep track of spawned beats
+        this.sweetSpotTolerance = 500;  // In milliseconds. Tolerance for early or late hits.
+    }
+
+    // Generate beats based on loadedBeats and current song time
+    spawnBeats(songTime) {
+        while (this.spawnedBeats < loadedBeats.length && loadedBeats[this.spawnedBeats] - this.sweetSpotTolerance <= songTime) {
+            let newYPosition = 0;  // Spawn at the top of the canvas
+            let newSpeed = 5;  // Pixels per frame, adjust as needed
+            this.beats.push(new Beat(newYPosition, newSpeed));
+            this.spawnedBeats++;
+        }
+    }
+
+    // Update the beats' positions and check for hits
+    moveBeats(songTime) {
+        for (let i = 0; i < this.beats.length; i++) {
+            let beat = this.beats[i];
+            beat.move();
+
+            // Change color based on proximity to the target time (sweet spot)
+           // if (Math.abs(loadedBeats[this.spawnedBeats - this.beats.length + i] - songTime) <= this.sweetSpotTolerance) {
+           //     beat.hit = true;
+           // } else {
+          //      beat.hit = false;
+          //  }
+
+            // Remove the beat if it's too far past the target area
+            if (loadedBeats[this.spawnedBeats - this.beats.length + i] + this.sweetSpotTolerance < songElapsedTime * 1000) {
+                this.beats.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    // Draw the beats
+    drawBeats(ctx) {
+        for (let beat of this.beats) {
+            beat.draw(ctx);
+        }
+    }
+
+    checkHitBeats(arg)
+    {
+        return true;
+    }
+
+    setBeatTimesFromJson(beatTimes) {
+        loadedBeats = beatTimes;
+    }
+}
+
+let beatManager = new BeatManager();
+
+
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -28,18 +91,13 @@ function loadSong(path) {
 }
 
 function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    let shouldPulse = false;
-    if (loadedBeats.length > 0 && songPlaying) {
-        for (let beat of loadedBeats) {
-            if (Math.abs(beat - songElapsedTime * 1000) <= 50) {  // Near a beat time
-                shouldPulse = true;
-                lastPulseTime = audioContext.currentTime;
-                break;
-            }
-        }
-    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+ 
+    beatManager.spawnBeats(songElapsedTime * 1000);
+    beatManager.moveBeats();
+    beatManager.drawBeats(ctx, songElapsedTime * 1000); 
+    
 
     let elapsedTimeSincePulse = audioContext.currentTime - lastPulseTime;
     if (elapsedTimeSincePulse <= pulseDuration / 1000) {
@@ -51,10 +109,9 @@ function render() {
     
 
     
-
-    let drawingRadius = circleRadius * currentPulseFactor;
+    drawingRadius = circleRadius * currentPulseFactor;
     ctx.beginPath();
-    ctx.arc(400, 300, drawingRadius, 0, 2 * Math.PI);
+    ctx.arc(960, 540, drawingRadius, 0, 2 * Math.PI); // Adjusted to canvas center
     ctx.strokeStyle = "green";
     ctx.stroke();
     
@@ -85,6 +142,7 @@ function render() {
     } else {
         songElapsedTime = songOffset / 1000;
     }
+
     const displayTime = songElapsedTime * 1000; // Convert to milliseconds
     ctx.fillText(Math.floor(displayTime) + " ms", 375, 370); // Adjusted y-coordinate
     requestAnimationFrame(render);
@@ -96,12 +154,11 @@ canvas.addEventListener("click", function(event) {
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
 
-    const distToCenter = Math.sqrt(Math.pow(400 - x, 2) + Math.pow(300 - y, 2));
-    if (distToCenter <= circleRadius && songPlaying) {
-        recordedMoments.push(Math.floor(songElapsedTime * 1000));
-        circleRadius = 60;
-        setTimeout(() => circleRadius = 50, 100);
+    if (beatManager.checkHitBeats(songElapsedTime * 1000)) {
+        currentPulseFactor = pulseFactor;
+        lastPulseTime = audioContext.currentTime;
     }
+
 
     if (x >= 350 && x <= 450 && y >= 400 && y <= 450) {
         if (!songPlaying) {
@@ -170,11 +227,64 @@ function loadBeatTimes() {
     reader.onload = function(event) {
         const jsonData = JSON.parse(event.target.result);
         if (jsonData && jsonData.beatTimes) {
-            loadedBeats = jsonData.beatTimes;
+            beatManager.setBeatTimesFromJson(jsonData.beatTimes);
+            beatManager.spawnBeats(7)
+
         } else {
             alert('Invalid JSON format.');
         }
     };
     reader.readAsText(file);
 }
+
+
+
+class Beat {
+    constructor(y, speed) {
+        this.x = canvas.width / 2;  // Middle of the canvas
+        this.y = y;  // Starting y-position (typically 0 for top)
+        this.radius = 20;  // Size of the circle
+        this.speed = speed;  // Pixels per frame
+        this.hit = false;  // If the player has successfully hit the beat
+    }
+
+    // Move the beat downwards
+    move() {
+        this.y += this.speed;
+    }
+
+    // Draw the beat
+    draw(ctx, currentTime) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.closePath();
+        
+        // Determine color based on proximity to sweet spot
+        const distanceFromSweetSpot = Math.abs(this.y - currentTime);
+        if (distanceFromSweetSpot <= this.radius * 2) { // Adjust the multiplier as needed for wider sweet spot
+            ctx.fillStyle = 'red';
+        } else {
+            ctx.fillStyle = this.hit ? 'green' : 'white';
+        }
+        
+        ctx.fill();
+    }
+    
+
+    // Check if beat is in 'sweet spot'
+    checkHit(targetY) {
+        const distance = Math.abs(this.y - targetY);
+        if (distance <= this.radius) {
+            this.hit = true;
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
 
