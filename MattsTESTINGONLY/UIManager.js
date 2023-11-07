55452d2c105bb4a0ac98398ae509dfc08b6dfb83
George Lambrakis <yorgo12345@hotmail.com>
2023-11-06 20:45:25 -0500

    Restores All the other folders ## MERGE ## # Conflicts: #	Hand_Of_God_OOP/GameManager.js #	Hand_Of_God_OOP/LevelSelectVisual.js #	Hand_Of_God_OOP/Level_05_Recorder.js #	Hand_Of_God_OOP/Level_Mp3AndJson/JustDance/JustDanceLevel.json #	Hand_Of_God_OOP/Level_StageSelect.js #	Hand_Of_God_OOP/MediaPipeTracker.js #	Hand_Of_God_OOP/OLD LEVEL TESTS/Level_01.js #	Hand_Of_God_OOP/OLD LEVEL TESTS/Level_02.js #	Hand_Of_God_OOP/OLD LEVEL TESTS/Level_03.js #	Hand_Of_God_OOP/OLD LEVEL TESTS/Level_04 copy.js #	Hand_Of_God_OOP/OLD LEVEL TESTS/Level_04.js #	Hand_Of_God_OOP/PlayInfoBoxVisual.js #	Hand_Of_God_OOP/UIManager.js #	Hand_Of_God_OOP/app.js #	Hand_Of_God_OOP/index.html

---
+++ MattsTESTINGONLY/UIManager.js
@@ -0,0 +1,208 @@
+export class Button 
+{
+    constructor(xRatio, yRatio, widthRatio, heightRatio, text) {
+        this.xRatio = xRatio;
+        this.yRatio = yRatio;
+        this.widthRatio = widthRatio;
+        this.heightRatio = heightRatio;
+        this.text = text;
+    }
+
+
+    
+    // Draw the button on a canvas context
+    draw(ctx, canvasWidth, canvasHeight, isHovered = false, isActive = false) {
+        const x = this.xRatio * canvasWidth;
+        const y = this.yRatio * canvasHeight;
+        const width = this.widthRatio * canvasWidth;
+        const height = this.heightRatio * canvasHeight;
+    
+        if (isActive) {
+            ctx.fillStyle = "#cccccc";  // Active state color
+            ctx.shadowColor = 'transparent';  // No shadow when button is pressed
+        } else {
+            ctx.fillStyle = isHovered ? "#e0e0e0" : "#f0f0f0";  // Normal/Hover state color
+            ctx.shadowColor = "rgba(0, 0, 0, 0.3)"; 
+            ctx.shadowBlur = 5;
+            ctx.shadowOffsetX = 3;  
+            ctx.shadowOffsetY = 3;  
+        }
+    
+        ctx.fillRect(x, y, width, height);
+        ctx.shadowColor = 'transparent';  // Reset shadow after drawing button
+    
+        ctx.fillStyle = "#000";  // Text color
+        ctx.font = "24px Arial"; 
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(this.text, x + width / 2, y + height / 2);
+    }
+    
+
+    // Check if a point is inside the button
+    isInside(x, y, canvasWidth, canvasHeight) {
+        const btnX = this.xRatio * canvasWidth;
+        const btnY = this.yRatio * canvasHeight;
+        const btnWidth = this.widthRatio * canvasWidth;
+        const btnHeight = this.heightRatio * canvasHeight;
+
+        return x > btnX && x < btnX + btnWidth && y > btnY && y < btnY + btnHeight;
+    }
+}
+
+
+//---------------------------
+// UIManager MANAGE CLASS
+//---------------------------
+export default class UIManager 
+{
+    constructor() 
+    {
+        this.buttons = 
+        {
+            startStopButton: new Button(0.1, 0.9, 0.1, 0.05, "Start Track"),
+            resetButton: new Button(0.3, 0.9, 0.1, 0.05, "Reset"),
+            exportBeatsButton: new Button(0.5, 0.9, 0.1, 0.05, "Export Beats"),
+            loadBeatsButton: new Button(0.7, 0.9, 0.1, 0.05, "Load Beats"),
+            loadSongButton: new Button(0.9, 0.9, 0.1, 0.05, "Load Song")
+
+        };
+
+        this.hoveredButton = null;
+        this.activeButton = null;
+
+        // Timer properties
+        this.timer = 0;  // in seconds
+        this.timerInterval = null;  // to hold the setInterval reference
+
+        //Load Song button properties
+        this.songInput = document.createElement('input');
+        this.songInput.type = 'file';
+        this.songInput.accept = '.mp3,.wav';
+        this.songInput.style.display = 'none';
+        document.body.appendChild(this.songInput);
+    }
+
+    startTimer() {
+        if (this.loadedSong) {
+            this.loadedSong.play();
+        }
+    }
+    
+
+    stopTimer() {
+        if (this.loadedSong) {
+            this.loadedSong.pause();
+        }
+    }
+
+   resetTimer() {
+    this.timer = 0;
+    if (this.loadedSong) {
+        this.loadedSong.currentTime = 0;
+    }
+}
+
+    drawAll(ctx, canvasWidth, canvasHeight, hoveredButton, activeButton) {
+        Object.values(this.buttons).forEach(btn => {
+            const isHovered = btn === hoveredButton;
+            const isActive = btn === activeButton;
+            btn.draw(ctx, canvasWidth, canvasHeight, isHovered, isActive);
+        });
+
+        // Draw timer above the start/stop button
+        const timerYPosition = this.buttons.startStopButton.yRatio * canvasHeight - 60;
+
+        // Calculate the x-position with an offset (adjust as needed)
+        const offset = -670; // This will move the text 50 pixels to the right
+        const timerXPosition = (canvasWidth / 2) + offset;
+
+        ctx.fillStyle = "#000";  // Text color
+        ctx.font = "24px Arial"; 
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+
+        // Format timer with two decimal places
+        const formattedTimer = this.loadedSong ? (this.loadedSong.currentTime).toFixed(2):0;
+        //const formattedTimer = this.loadedSong.currentTime.toFixed(2);
+
+        ctx.fillText(`${formattedTimer}s`, timerXPosition, timerYPosition);
+
+
+    }
+
+
+    //logic for what to do with buttons when mouse is clicked
+    handleCanvasClick(x, y, canvasWidth, canvasHeight) {
+        for (const [buttonName, button] of Object.entries(this.buttons)) {
+            if (button.isInside(x, y, canvasWidth, canvasHeight)) {
+                console.log(`${buttonName} clicked!`);
+    
+                if (buttonName === "startStopButton") {
+                    if (button.text === "Start Track") {
+                        button.text = "Stop Track";
+                        this.startTimer();
+                    } else {
+                        button.text = "Start Track";
+                        this.stopTimer();
+                    }
+                } else if (buttonName === "resetButton") {
+                    this.resetTimer();
+                } // ... other button logic remains unchanged
+
+                else if (buttonName === "loadSongButton") 
+                {
+                    this.songInput.click();
+                }
+            }
+        }
+    }
+
+    
+
+
+
+    
+    //handing the varios mouse position and clicks on the canvus:
+    getRelativeMousePosition(e) {
+        const rect = e.target.getBoundingClientRect();
+        return {
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top
+        };
+    }
+ 
+    handleMouseMove(e) {
+        const position = this.getRelativeMousePosition(e);
+        this.hoveredButton = Object.values(this.buttons).find(btn => btn.isInside(position.x, position.y, e.target.width, e.target.height));
+    }
+ 
+    handleMouseDown(e) {
+        console.log("Mouse down detected")
+        const position = this.getRelativeMousePosition(e);
+        this.activeButton = Object.values(this.buttons).find(btn => btn.isInside(position.x, position.y, e.target.width, e.target.height));
+        
+    }
+
+ 
+    handleMouseUp() {
+        this.activeButton = null;
+    }
+ 
+    handleClick(e) {
+        const position = this.getRelativeMousePosition(e);
+        this.handleCanvasClick(position.x, position.y, e.target.width, e.target.height);
+    }
+
+    handleSongInput(e) {
+        const files = e.target.files;
+        if (files.length === 0) return;
+    
+        const selectedFile = files[0];
+        this.loadedSong = new Audio(URL.createObjectURL(selectedFile));
+    }
+    
+
+}
+
+
