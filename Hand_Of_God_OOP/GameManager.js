ec3e5d4730fe13d996d94fc5282d1802dc4eb3b6
momentollogy <102498438+momentollogy@users.noreply.github.com>
2023-11-07 15:18:43 -0500

    Level 5 now read full LevelArray BUT never got leaderboard per level to pass to Leaderboard Visual Yet.

---
+++ Hand_Of_God_OOP/GameManager.js
@@ -0,0 +1,32 @@
+
+
+import Level_05 from './Level_05_Recorder.js'
+//import JustDance from './Level_JustDance.js'
+import Level_StageSelect from './Level_StageSelect.js'
+
+export default class GameManager {
+    constructor() 
+    {
+    this.currentLevel = new Level_StageSelect()
+      // this.currentLevel = new Level_05()
+       //this.currentLevel = new JustDance()
+     //  document.addEventListener('levelChange', this.handleLevelChange.bind(this));
+        document.addEventListener('levelChange', async (event) => {
+        const levelName = event.detail.levelName; // This would be a string like 'JustDance'
+        try {
+          const module = await import(`./${levelName}.js`);
+          const LevelClass = module.default; // Assumes the class is the default export
+          const levelInstance = new LevelClass(event.detail.mp3Path, event.detail.jsonPath);
+          this.currentLevel.dispose();
+          this.currentLevel = null;
+          this.currentLevel=levelInstance;
+
+          // Now use levelInstance as needed
+        } catch (error) {
+          console.error('Failed to load level module:', error);
+        }
+      });
+
+    }    
+}
+
---
+++ Hand_Of_God_OOP/Level_BasicTouch.js
@@ -0,0 +1,190 @@
+import MediaPipeTracker from './MediaPipeTracker.js';
+import UIManager from './UIManager.js';
+import SweetSpotCircle from './SweetSpotCircle.js';
+import JsonManager from './JsonManager.js';
+import DrawEngine from './DrawEngine.js';
+import LeaderBoardVisual from './LeaderBoardVisual.js';
+import { addScore } from './Leaderboard.js';
+ 
+
+
+export default class Level_BasicTouch
+{
+    constructor(_mp3Path,_jsonPath)
+    {
+        this.mediaPipe = MediaPipeTracker.getInstance()
+        this.canvas = document.getElementById("output_canvas");;
+        this.ctx = this.canvas.getContext("2d");
+        this.drawEngine = DrawEngine.getInstance();
+        this.fileInput = document.getElementById('fileInput');
+        
+        this.audio = new Audio();
+        this.audio.volume = 0.03; 
+        
+        this.jsonManager = new JsonManager();
+        this.mp3Path=_mp3Path;
+        this.jsonPath=_jsonPath;
+        this.fireBaseLevelLeaderBoard = _fireBaseLevelLeaderBoard;
+        
+        this.uIButtons = []
+        this.uiManager = new UIManager(this.audio,this.uIButtons)
+
+        this.SweetSpotCircleArray=[];
+        this.SweetSpotCircleArray[0] = new SweetSpotCircle(this.audio,  'rgb(0, 255, 0)',     { x: this.canvas.width /2 -125, y: this.canvas.height/2+200}  );
+        this.SweetSpotCircleArray[1] = new SweetSpotCircle(this.audio,  'rgb(0, 255, 200)',   { x: this.canvas.width /2 +125, y: this.canvas.height/2+200} );
+        this.SweetSpotCircleArray[0].beatCirclePathDirectionAngle = -90;
+        this.SweetSpotCircleArray[1].beatCirclePathDirectionAngle = -90;
+        this.SweetSpotCircleArray[0].name="LeftSSCir";
+        this.SweetSpotCircleArray[1].name="RightSSCir";
+
+        this.recordedMoments_Array=[];
+        this.recordMode = false;
+        this.beatsMissed=0;
+        this.scoreNumber = 0;
+        this.comboNumber = 0;
+
+        this.beatsMissedPrevious=0;
+        this.beatArray=[];
+        this.beatCircles_Array = [];
+        this.lastfingerposition;
+        this.prevHandPositions=[];
+        this.handInsidePreviously = false;
+        this.swipePositions = [];
+        this.swipeDirectionPos_arr = [];
+        this.swipeHand = "";
+        this.currentHandedness = null;
+        this.previousPositions_L_arr=[];
+        this.previousPositions_R_arr=[];
+        this.displayBkg = false;
+        this.hasBeatBeenMissed=false;
+
+        this.playerName = 'momentology'; // Add this line with a default test player name
+      //  const leaderBoardVisual = new LeaderBoardVisual();
+        this.leaderBoardVisualInstance = new LeaderBoardVisual();
+
+
+        // event handler for when json is fully loaded
+        document.addEventListener('beatTimeDataReady', event => {
+            this.SweetSpotCircleArray[0].receiveAndProcessCircleData(this.jsonManager.leftCircleData);
+            this.SweetSpotCircleArray[1].receiveAndProcessCircleData(this.jsonManager.rightCircleData);
+        });
+        
+        // event handler for when beat missed ( this is dispatched from SweetSpotCircles )
+        document.addEventListener("BeatMissed", (data) => {;
+            this.beatMissed();
+        });
+
+        // listen for when song ends to log level complete
+        this.audio.addEventListener('ended', this.audioEnded.bind(this));
+
+        
+
+        /*
+        //event listner for volume knob
+        this.canvas.addEventListener('click', (event) => {
+            this.uiManager.handleVolumeClick(event);
+        });
+        */
+        
+        // load mp3, json, and play
+        this.audio.src = this.mp3Path;          
+        this.jsonManager.loadJsonFileByPath(this.jsonPath);
+        this.audio.play();
+    }
+
+    
+    level_loop() {
+        // mediapipe stuff
+        let results = this.mediaPipe.results;
+        if (results == undefined) { return; }
+        
+        if(!this.recordMode){
+            // game interaction and score stuff
+            this.checkForFingerTouchCircles();
+            //this.checkCirclesForMissesAndStuff();
+        }else{
+            this.sendTouchesForRecording();
+        }
+
+        // update display stuff and process classes stuff
+        for(let sweetspotcircle of this.SweetSpotCircleArray) { sweetspotcircle.updateAndDraw(); }
+        this.uiManager.draw();
+    }
+    
+    checkForFingerTouchCircles(){
+        for(let sweetspotcircle of this.SweetSpotCircleArray){
+            if (this.mediaPipe.checkForTouchWithShape(sweetspotcircle, this.mediaPipe.BOTH,  8).length>0)
+            {
+                sweetspotcircle.puffy = true;  
+                let percentAccuracyIfTouched = sweetspotcircle.touch(); // this method returns null if touch is invalid
+                if(percentAccuracyIfTouched){
+                    this.touchSuccesfulWithPercentage(percentAccuracyIfTouched, sweetspotcircle);
+                }
+            }else{
+                sweetspotcircle.puffy = false;
+            }
+        }
+    }
+
+    increaseComboNumer(){
+        this.comboNumber += 1;
+        this.uiManager.comboNumber = this.comboNumber;
+    }
+
+    resetComboNumber(){
+        this.comboNumber = 0;
+        this.uiManager.comboNumber = this.comboNumber;
+    }
+
+    removeMiss(){
+        if(this.beatsMissed>0){this.beatsMissed -= 1;}
+        this.uiManager.missesNumber = this.beatsMissed;
+    }
+    
+    
+
+
+    touchSuccesfulWithPercentage(percentAccuracy, sweetspotcircle)
+    {
+        ////////////////////////////////////////////////////////////////////
+        ////////////// Touch Succesful. Receive Percent ////////////////////
+        //////////////////////////////////////////////////////////////////// 
+        this.increaseComboNumer(); 
+        this.scoreNumber += ( percentAccuracy + this.comboNumber );
+        this.uiManager.scoreNumber = this.scoreNumber;
+        this.removeMiss();
+        //console.log(percentAccuracy + "%  accuracy", sweetspotcircle.color, "score:", this.scoreNumber);
+    }
+
+    beatMissed()
+    {
+        ////////////////////////////////////////////////////////////////////
+        ////////////// Beat Missed. Total Beats Tallied ////////////////////
+        ////////////////////////////////////////////////////////////////////
+        this.beatsMissed += 1;
+        this.uiManager.missesNumber = this.beatsMissed;
+        this.resetComboNumber();
+        //console.log(this.beatsMissed + " Beats Missed total");
+        if(this.beatsMissed > 20){
+            console.log("you lose");
+            this.audio.pause();
+            // show something in the UI perhaps?
+        }
+    }
+
+
+    audioEnded() {
+        console.log('Level Complete');
+        console.log('Score is:', this.scoreNumber);
+    
+        addScore(this.playerName, this.scoreNumber).then(() => {
+            this.leaderBoardVisualInstance.populateAndDraw();
+        }).catch(error => {
+            console.error("Error adding score: ", error);
+        });
+    }
+    
+
+}
+
+
