c07a73a0d91290ba229bd505b4f260db343c0388
momentollogy <102498438+momentollogy@users.noreply.github.com>
2023-11-22 16:38:48 -0500

    Working. Firebase writes Optimize using local caching. Favicon added!

--- Hand_Of_God_OOP/LeaderBoard_Box.js
+++ Hand_Of_God_OOP/LeaderBoard_Box.js
@@ -1,7 +1,7 @@
 // LeaderBoard_Box.js
 
 import BoxUI from './BoxUI.js';
-import { getTopScores, getLatestScore, getPlayerTopScores } from './Leaderboard.js'; // Adjust the path if necessary
+import { getCachedTopScores, getCachedLatestScore, getCachedPlayerTopScores} from './Leaderboard.js'; // Adjust the path if necessary
 
 
 export default class LeaderBoard_Box 
@@ -274,18 +274,8 @@ export default class LeaderBoard_Box
     {
         this.currentSongData = songData;
     }
-/*
-    setState(fb,LB_state)
-    {
-        //console.log(LB_state)
-        this.populateAndDraw(fb,LB_state);
-        
-    }
-    */
-
     
 
-// working Draw
     draw() 
     {
         this.ctx.save();
@@ -371,61 +361,69 @@ export default class LeaderBoard_Box
 
     
 
-   
 
     currentFireBaseLevelLeaderBoard = null;
 
 
-    // Modify levelSelected to update currentFireBaseLevelLeaderBoard
+
+
     levelSelected(event) {
         this.currentFireBaseLevelLeaderBoard = event.detail.fireBaseLevelLeaderBoard;
+    
+        // Ensuring the cache is populated for the selected level
+        getCachedTopScores(this.currentFireBaseLevelLeaderBoard).then(() => {
+        });
+        getCachedLatestScore(this.currentFireBaseLevelLeaderBoard).then(() => {
+        });
+        getCachedPlayerTopScores(this.currentFireBaseLevelLeaderBoard).then(() => {
+        });
+    
+        // Then draw the leaderboard
         this.populateAndDraw(this.currentFireBaseLevelLeaderBoard, 'topScores');
     }
+    
+
+
 
 
 
     async populateAndDraw(fireBaseLevelLeaderBoard, dataType='topScores') {
-        console.log("populateAndDraw called with:", fireBaseLevelLeaderBoard, dataType); // Log received parameters
-
+    
         if (!fireBaseLevelLeaderBoard) {
             fireBaseLevelLeaderBoard = this.currentFireBaseLevelLeaderBoard || "Default_Leaderboard_ID";
         }
-
+    
         let transformedScores;
     
         switch (dataType) {
             case 'topScores': {
-                console.log("Fetching topScores with:", fireBaseLevelLeaderBoard); // Log parameter for Firebase call
-
-                let topScores = await getTopScores(fireBaseLevelLeaderBoard); 
+    
+                // Replace getTopScores with getCachedTopScores
+                let topScores = await getCachedTopScores(fireBaseLevelLeaderBoard); 
                 transformedScores = topScores.map(score => ({
                     playerName: score.name,
                     score: score.score
                 }));
                 break;
             }
-
-
+    
             case 'latestScores': {
-                console.log("Fetching latestScores with:", fireBaseLevelLeaderBoard); // Log parameter for Firebase call
             
-                let latestScoreData = await getLatestScore(fireBaseLevelLeaderBoard);
-                // Use the rank directly from the score data, no need to adjust it here
+                // Replace getLatestScore with getCachedLatestScore
+                let latestScoreData = await getCachedLatestScore(fireBaseLevelLeaderBoard);
                 transformedScores = latestScoreData.surroundingScores.map(score => ({
-                    rank: score.rank, // Use the assigned rank directly
+                    rank: score.rank,
                     playerName: score.name,
                     score: score.score,
-                    isLatest: score.isLatest // Use the isLatest flag from the score data
+                    isLatest: score.isLatest
                 }));
                 break;
             }
-            
-
-
+    
             case 'PlayerTopScores': {
-                console.log("Fetching PlayerTopScores with:", fireBaseLevelLeaderBoard); // Log parameter for Firebase call
-
-                let playerTopScores = await getPlayerTopScores(fireBaseLevelLeaderBoard);
+    
+                // Replace getPlayerTopScores with getCachedPlayerTopScores
+                let playerTopScores = await getCachedPlayerTopScores(fireBaseLevelLeaderBoard);
                 transformedScores = playerTopScores.map(score => ({
                     playerName: score.name,
                     score: score.score
@@ -437,10 +435,11 @@ export default class LeaderBoard_Box
         this.update(transformedScores);
         
     }
-
+    
     update(newScores) {
         this.scores = newScores;
     }
+    
 
     dispose() {
         console.log("Disposing Leaderboard_Box...")
@@ -452,250 +451,11 @@ export default class LeaderBoard_Box
         window.removeEventListener('levelSelected', this.boundLevelSelected);
         this.canvas.removeEventListener('click', this.boundHandleButtonClick);
         this.canvas.removeEventListener('mousemove', this.boundHandleMouseMove);
-       // this.canvas = null;
-      //  this.ctx = null;
-
-          // Remove event listeners using the stored bound functions
-      
+           
 
       }
+
+
+
 }
-
-
-
-/*
-
-// LeaderBoard_Box.js
-// Class for visualizing the leaderboard, including text and interactions with Firebase.
-
-import BoxUI from './BoxUI.js';
-import { getTopScores, getLatestScore, getPlayerTopScores } from './Leaderboard.js'; // Adjust the path if necessary
-
-
-export default class LeaderBoard_Box {
-    constructor() {
-        this.scores = []; // Placeholder for scores
-        this.canvas = document.getElementById("output_canvas");
-        this.ctx = this.canvas.getContext("2d");
-    
-        this.currentSongData;
-
-        this.boundLevelSelected = this.levelSelected.bind(this);
-        window.addEventListener('levelSelected', this.boundLevelSelected);
-
-        // Box properties
-        this.RESIZE_FACTOR = 0.8;
-        this.POSITION_OFFSET_X = -610;
-        this.POSITION_OFFSET_Y = 0;
-        this.LEADERBOARD_WIDTH = 700 * this.RESIZE_FACTOR;
-        this.LEADERBOARD_HEIGHT = 800;
-        this.LEADERBOARD_X = ((1920 - this.LEADERBOARD_WIDTH) / 2) + this.POSITION_OFFSET_X;
-        this.LEADERBOARD_Y = ((1080 - this.LEADERBOARD_HEIGHT) / 2) + this.POSITION_OFFSET_Y;
-        this.LEADERBOARD_RADIUS = 100;
-        
-        // Header and score styling variables
-        this.HEADER_HEIGHT = 155;
-        this.HEADER_FONT_SIZE = 50;
-        this.SCORE_FONT_SIZE = 30;
-        this.SCORE_LINE_HEIGHT = 50;
-        this.SCORE_MARGIN_LEFT = 50;
-        this.SCORE_MARGIN_RIGHT = 50;
-
-        // Create BoxUI instance
-        this.leaderboardBox = new BoxUI(
-            this.ctx,
-            this.LEADERBOARD_X,
-            this.LEADERBOARD_Y,
-            this.LEADERBOARD_WIDTH,
-            this.LEADERBOARD_HEIGHT,
-            this.LEADERBOARD_RADIUS
-            // No resize factor or offsets here, as they are already calculated in width and X, Y positions.
-        );
-
-             //   const defaultLeaderboardId = "JustDance_easy_LeaderBoard";
-
-
-        // Immediately attempt to populate and draw the leaderboard
-   // this.populateAndDraw(defaultLeaderboardId, detail.leaderBoardState); //or latestScores or playerTopscores   
-      // this.setState(detail.leaderBoardState);
-    }
-
-    setCurrentSongLevel(songData)
-    {
-        this.currentSongData = songData;
-    }
-
-    setState(fb,LB_state)
-    {
-        //console.log(LB_state)
-        this.populateAndDraw(fb,LB_state);
-        
-    }
-
-    //window.myCurrentLevelIndex = 0;
-
-   
-
-    draw() {
-
-      //  console.log("Drawing leaderboard with scores: ", this.scores);
-
-        this.ctx.save();
-        // Set the style for the box
-        this.ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-        this.leaderboardBox.draw();
-    
-        // Draw the HIGHSCORES header
-        this.ctx.font = `${this.HEADER_FONT_SIZE}px Verdana`;
-        this.ctx.fillStyle = "white";
-        this.ctx.textAlign = "center";
-        this.ctx.fillText(
-            "HIGHSCORES", 
-            this.LEADERBOARD_X + (this.LEADERBOARD_WIDTH / 2), 
-            this.LEADERBOARD_Y + this.HEADER_HEIGHT / 2 + 37
-        );
-    
-        // Start of new code for drawing the scores
-        let startY = this.LEADERBOARD_Y + this.HEADER_HEIGHT + this.SCORE_LINE_HEIGHT;
-    
-        if (this.scores && this.scores.length > 0) {
-            // Check if the scores include a 'rank' property
-            let includesRank = 'rank' in this.scores[0];
-    
-            for (let i = 0; i < this.scores.length && i < 10; i++) {
-                let scoreItem = this.scores[i];
-
-               // console.log(`Setting fillStyle for ${scoreItem.playerName}: `, scoreItem.isLatest ? "red" : "white");
-
-    
-                //  the rank or index
-                this.ctx.font = `${this.SCORE_FONT_SIZE}px Verdana`;
-               // this.ctx.fillStyle = "white";
-               this.ctx.fillStyle = scoreItem.isLatest ? "red" : "white";
-
-    
-                if (includesRank) {
-                    this.ctx.textAlign = "left";
-                    this.ctx.fillText(
-                        scoreItem.rank.toString(), 
-                        this.LEADERBOARD_X + this.SCORE_MARGIN_LEFT, 
-                        startY + (i * this.SCORE_LINE_HEIGHT)
-                    );
-                } else {
-                    this.ctx.textAlign = "left";
-                    this.ctx.fillText(
-                        (i + 1).toString(), 
-                        this.LEADERBOARD_X + this.SCORE_MARGIN_LEFT, 
-                        startY + (i * this.SCORE_LINE_HEIGHT)
-                    );
-                }
-    
-                // Draw player name
-                this.ctx.textAlign = "center";
-                this.ctx.fillText(
-                    scoreItem.playerName, 
-                    this.LEADERBOARD_X + (this.LEADERBOARD_WIDTH / 2), 
-                    startY + (i * this.SCORE_LINE_HEIGHT)
-                );
-    
-                // Draw score
-                this.ctx.textAlign = "right";
-                this.ctx.fillText(
-                    scoreItem.score.toString(), 
-                    this.LEADERBOARD_X + this.LEADERBOARD_WIDTH - this.SCORE_MARGIN_RIGHT, 
-                    startY + (i * this.SCORE_LINE_HEIGHT)
-                );
-            }
-        }
-    
-        this.ctx.restore();
-    }
-
-    
-
-    // Handle the levelSelected event
-    levelSelected(event) 
-    {
-        const selectedLevelLeaderboard = event.detail.fireBaseLevelLeaderBoard;
-        this.populateAndDraw(selectedLevelLeaderboard, 'topScores');
-       // console.log(`Drawing score for ${scoreItem.playerName}, isLatest in levelSelected loop: `, scoreItem.isLatest);
-
-    }
-    //topScores   latestScores //topPlayerscores
-
-
-
-    async populateAndDraw(fireBaseLevelLeaderBoard, dataType) {
-     // console.log("right after pop and draw",dataType)
-
-        let transformedScores;
-    
-        switch (dataType) {
-            case 'topScores': {
-                let topScores = await getTopScores(fireBaseLevelLeaderBoard); 
-                transformedScores = topScores.map(score => ({
-                    playerName: score.name,
-                    score: score.score
-                }));
-                break;
-            }
-
-
-            case 'latestScores': {
-                let latestScoreData = await getLatestScore(fireBaseLevelLeaderBoard);
-                transformedScores = latestScoreData.surroundingScores.map((score, index) => {
-                    let rankAdjustment = index - latestScoreData.surroundingScores.indexOf(latestScoreData.recentScoreData);
-                    let rank = latestScoreData.rank + rankAdjustment;
-                    return {
-                        rank: rank,
-                        playerName: score.name,
-                        score: score.score,
-                        isLatest: index === latestScoreData.surroundingScores.indexOf(latestScoreData.recentScoreData)
-                    };
-                });
-                break;
-            }
-
-
-            case 'PlayerTopScores': {
-                let playerTopScores = await getPlayerTopScores(fireBaseLevelLeaderBoard);
-                transformedScores = playerTopScores.map(score => ({
-                    playerName: score.name,
-                    score: score.score
-                }));
-                break;
-            }
-        }
-    
-        this.update(transformedScores);
-        this.draw();
-    }
-
-    update(newScores) {
-        this.scores = newScores;
-    }
-
-    dispose() {
-        window.removeEventListener('levelSelected', this.boundLevelSelected);
-      }
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- Hand_Of_God_OOP/Leaderboard.js
+++ Hand_Of_God_OOP/Leaderboard.js
@@ -4,8 +4,13 @@
 import { db } from './firebase.js';
 import { collection, query, orderBy, limit, getDocs, where, addDoc,startAt, endBefore, startAfter } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
+const leaderboardCache = {
+  topScores: {},
+  latestScores: {},
+  playerTopScores: {}
+};
 
-
+/*
 export async function addScore(name, score, levelArrayDataObject) {
   try {
     // Add the score to the database.
@@ -29,13 +34,62 @@ export async function addScore(name, score, levelArrayDataObject) {
       rank++; // Increment rank until the new score is found.
     }
 
+    
+ // Cache update after adding a new score
+ leaderboardCache.topScores[levelArrayDataObject.fireBaseLevelLeaderBoard] = await getCachedTopScores(levelArrayDataObject.fireBaseLevelLeaderBoard, 10);
+
+ leaderboardCache.latestScores[levelArrayDataObject.fireBaseLevelLeaderBoard] = await getCachedLatestScore(levelArrayDataObject.fireBaseLevelLeaderBoard);
+
+ leaderboardCache.playerTopScores[levelArrayDataObject.fireBaseLevelLeaderBoard] = await getCachedPlayerTopScores(levelArrayDataObject.fireBaseLevelLeaderBoard);
+
     // Return the document reference ID and the rank.
     return { id: docRef.id, rank: rank };
   } catch (e) {
     console.error("Error adding score: ", e);
     throw e; // Re-throw the error to handle it in the calling function.
   }
+
 }
+*/
+
+//RANK NOT DEFINED - fix first
+export async function addScore(name, score, levelArrayDataObject) {
+  try {
+      // Add the score to the database.
+      const docRef = await addDoc(collection(db, levelArrayDataObject.fireBaseLevelLeaderBoard), {
+          name: name,
+          score: score,
+          timestamp: new Date()
+      });
+
+      // After adding the score, calculate the rank.
+      const scoresQuery = query(collection(db, levelArrayDataObject.fireBaseLevelLeaderBoard), orderBy("score", "desc"));
+      const scoresSnapshot = await getDocs(scoresQuery);
+      
+      // Initialize rank at 1.
+      let rank = 1;
+      for (const doc of scoresSnapshot.docs) {
+          if (doc.id === docRef.id) {
+              // This is the rank of the newly added score.
+              break;
+          }
+          rank++; // Increment rank until the new score is found.
+      }
+
+      // Update the cache with the latest data for each leaderboard state
+      // This ensures the cache includes the newly added score
+      leaderboardCache.topScores[levelArrayDataObject.fireBaseLevelLeaderBoard] = await getTopScores(levelArrayDataObject.fireBaseLevelLeaderBoard, 10);
+      leaderboardCache.latestScores[levelArrayDataObject.fireBaseLevelLeaderBoard] = await getLatestScore(levelArrayDataObject.fireBaseLevelLeaderBoard);
+      leaderboardCache.playerTopScores[levelArrayDataObject.fireBaseLevelLeaderBoard] = await getPlayerTopScores(levelArrayDataObject.fireBaseLevelLeaderBoard);
+
+      // Return the document reference ID and the rank.
+      return { id: docRef.id, rank: rank };
+  } catch (e) {
+      console.error("Error adding score: ", e);
+      throw e; // Re-throw the error to handle it in the calling function.
+  }
+}
+
 
 
 
@@ -57,6 +111,29 @@ export async function getTopScores(leaderboardId,limitCount = 10) {
     return []; // Return an empty array on error
   }
 }
+
+
+
+
+// CACHE TOP SCORES 
+export async function getCachedTopScores(leaderboardId, limitCount = 10) {
+  if (leaderboardCache.topScores[leaderboardId]) {
+    console.log("Cache used for topScores");
+
+    return leaderboardCache.topScores[leaderboardId];
+  }
+
+  console.log("Firebase called for topScores");
+
+  const scores = await getTopScores(leaderboardId, limitCount);
+  leaderboardCache.topScores[leaderboardId] = scores;
+  return scores;
+}
+
+
+
+
+
 
 
 // Function to retrieve the most recent score, rank it, and show surrounding scores
@@ -101,69 +178,21 @@ export async function getLatestScore(leaderboardId) {
 }
 
 
+// CACHE LATESTSCORES 
+export async function getCachedLatestScore(leaderboardId) {
+  if (leaderboardCache.latestScores[leaderboardId]) {
+    console.log("Cache used for latestScores");
 
+    return leaderboardCache.latestScores[leaderboardId];
+  }
 
+  console.log("Firebase called for latestScores");
 
-
-
-/*
-//WOrking logging!
-export async function getLatestScore(leaderboardId) {
-  // console.log("Recent score data: ", recentScoreData);
- 
-   // Query to get the most recent score
-   const recentScoreQuery = query(collection(db, leaderboardId), orderBy("timestamp", "desc"), limit(1));
-   const recentScoreSnapshot = await getDocs(recentScoreQuery);
-   const recentScoreData = recentScoreSnapshot.docs[0].data();
-   recentScoreData.isLatest = true; // Add this line
- 
- 
-   // Compute the rank of the recent score
-   const higherScoresQuery = query(collection(db, leaderboardId), orderBy("score", "desc"), where("score", ">", recentScoreData.score));
-   const higherScoresSnapshot = await getDocs(higherScoresQuery);
-   const rank = higherScoresSnapshot.size + 1;
- 
-
-
-  // Fetch scores above the recent score
-const scoresAboveQuery = query(collection(db, leaderboardId), orderBy("score", "desc"), 
-where("score", ">", recentScoreData.score), limit(4));
-const scoresAboveSnapshot = await getDocs(scoresAboveQuery);
-
-// Fetch scores below the recent score
-const scoresBelowQuery = query(collection(db, leaderboardId), orderBy("score"), 
-where("score", "<", recentScoreData.score), limit(5));
-const scoresBelowSnapshot = await getDocs(scoresBelowQuery);
-
-// Combine the scores with the recent score in the middle
-let surroundingScores = [];
-
-// Corrected logging for scores above
-const scoresAbove = [];
-scoresAboveSnapshot.forEach(doc => {
-scoresAbove.unshift(doc.data()); // This will reverse the order
-});
-for (let i = 0; i < scoresAbove.length; i++) {
-console.log(`Rank for ${scoresAbove[i].name} with score ${scoresAbove[i].score}: ${rank - (scoresAbove.length - i)}`);
+  const latestScore = await getLatestScore(leaderboardId);
+  leaderboardCache.latestScores[leaderboardId] = latestScore;
+  return latestScore;
 }
 
-surroundingScores.push(...scoresAbove);
-surroundingScores.push(recentScoreData); // Add the most recent score
-console.log(`Rank for ${recentScoreData.name} with score ${recentScoreData.score}: ${rank}`);
-
-// Corrected logging for scores below
-const scoresBelow = scoresBelowSnapshot.docs.map(doc => doc.data());
-for (let i = 0; i < scoresBelow.length; i++) {
-console.log(`Rank for ${scoresBelow[i].name} with score ${scoresBelow[i].score}: ${rank + i + 1}`);
-}
-
-surroundingScores.push(...scoresBelow);
-
-return { surroundingScores, rank };
-
-
-}
-*/
 
 
 
@@ -199,3 +228,14 @@ export async function getPlayerTopScores(leaderboardId) {
   return scores;
 }
 
+
+// Place this right after the existing getPlayerTopScores function
+export async function getCachedPlayerTopScores(leaderboardId) {
+  if (leaderboardCache.playerTopScores[leaderboardId]) {
+    return leaderboardCache.playerTopScores[leaderboardId];
+  }
+
+  const playerScores = await getPlayerTopScores(leaderboardId);
+  leaderboardCache.playerTopScores[leaderboardId] = playerScores;
+  return playerScores;
+}
