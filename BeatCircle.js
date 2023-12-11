21aeebbb0038d990f67d0bc8eed0e88a3535fcb6
momentollogy <102498438+momentollogy@users.noreply.github.com>
2023-12-11 09:28:21 -0500

    Added Swipes to some levels need to clean up.

--- BeatCircle.js
+++ BeatCircle.js
@@ -59,67 +59,6 @@ update(audioCurrentTime, velocity, _angle) {
 
 
     
-
-
-
-
-
-//Geroges original
-/*
-    draw(){
-        this.ctx.save();
-
-        this.ctx.globalAlpha = this.alpha;
-
-        var WedgeAngle = .785; 
-        const startAngle = WedgeAngle * 1.1;
-        const endAngle = WedgeAngle * 2.9;
-
-        const flatTopX1 = this.x + this.radius * Math.cos(startAngle);
-        const flatTopY1 = this.y + this.radius * Math.sin(startAngle);
-        const flatTopX2 = this.x + this.radius * Math.cos(endAngle);
-        const flatTopY2 = this.y + this.radius * Math.sin(endAngle);
-        this.ctx.beginPath();
-        this.ctx.moveTo(flatTopX1, flatTopY1);
-        this.ctx.lineTo(this.x,this.y + this.radius /3);//maybe?
-        this.ctx.lineTo(flatTopX2, flatTopY2 );
-        this.ctx.arc(this.x, this.y, this.radius, startAngle, endAngle);
-        this.ctx.fillStyle = this.directionIndicatorColor;
-        this.ctx.lineWidth = 4;
-        this.ctx.fill();
-        //this.ctx.stroke();
-
-        this.ctx.fillStyle = this.color;
-        this.ctx.globalAlpha = this.alpha;
-        
-        // outer stroke
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = this.color;
-        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        this.ctx.lineWidth = this.lineWidth;
-        this.ctx.stroke();
-        
-        // inner stroke
-        this.ctx.beginPath();
-        this.ctx.globalAlpha = Math.min(Math.max(this.alpha-.5, 0), 1);
-        this.ctx.strokeStyle = this.color;
-        this.ctx.arc(this.x, this.y, this.radius-this.lineWidth*2, 0, 2 * Math.PI);
-        this.ctx.lineWidth = this.lineWidth * 2 - 2;
-        this.ctx.stroke();
-        
-        // inner fill
-        this.ctx.globalAlpha = Math.min(Math.max(this.alpha-.85, 0), 1);;
-        //this.ctx.fillStyle = "rgb(180,0,255)";
-        this.ctx.fill();
-        this.ctx.font = "20px Arial";
-        //this.ctx.fillText(this.swipeData, this.x + 100, this.y);
-        this.ctx.globalAlpha = 1.0;
-
-        this.ctx.restore();
-    }
-
-*/
-
 draw() {
     this.ctx.save();
 
@@ -180,59 +119,6 @@ draw() {
 
         this.ctx.restore();
     }
-
-
-
-
-
-
-
-
- /*   
-//Arrow Test 180 turns it left
-draw() {
-    this.ctx.save();
-
-    // Scale variable to adjust the overall size of the arrow
-    const scale = 1; // Change this value to scale the size of the arrow
-
-    // Angle variable in degrees to set the orientation of the arrow (0 is up, 90 is right, 180 is down, 270 or -90 is left)
-    const angleDegrees = 180; // Set this to -90 to make the arrow face left
-
-    // Convert degrees to radians for rotation
-    const rotation = angleDegrees * Math.PI / 180;
-
-    // Arrow dimensions, scaled
-    const arrowLength = this.radius * 2 * scale; // Length of the arrow shaft
-    const arrowWidth = this.radius / 2 * scale; // Width of the arrow shaft
-    const headLength = this.radius * 0.75 * scale; // Length of the arrow head
-    const headWidth = this.radius * scale; // Width of the arrow head
-
-    // Translate to the center point for rotation
-    this.ctx.translate(this.x, this.y);
-
-    // Rotate the arrow to the specified orientation
-    this.ctx.rotate(rotation);
-
-    // Create arrow shaft
-    this.ctx.beginPath();
-    this.ctx.moveTo(-arrowLength / 2, -arrowWidth / 2);
-    this.ctx.lineTo(-arrowLength / 2, arrowWidth / 2);
-    this.ctx.lineTo(-arrowLength / 2 + arrowLength - headLength, arrowWidth / 2);
-    this.ctx.lineTo(-arrowLength / 2 + arrowLength - headLength, headWidth / 2);
-    this.ctx.lineTo(-arrowLength / 2 + arrowLength, 0); // Tip of the arrow
-    this.ctx.lineTo(-arrowLength / 2 + arrowLength - headLength, -headWidth / 2);
-    this.ctx.lineTo(-arrowLength / 2 + arrowLength - headLength, -arrowWidth / 2);
-    this.ctx.closePath();
-
-    // Style the arrow with a white stroke
-    this.ctx.strokeStyle = 'white';
-    this.ctx.lineWidth = 4 * scale; // Width of the stroke scaled
-    this.ctx.stroke();
-
-    this.ctx.restore();
-}
-*/
 
 
 
--- SweetSpotCircle.js
+++ SweetSpotCircle.js
@@ -3,7 +3,7 @@ import DrawEngine from './DrawEngine.js';
 
 export default class SweetSpotCircle {
     
-    constructor(level, color='rgb(0,0,255)', position = {x:1000,y:200}, radius = 120, thickness = 2, is_growing = false, is_moving = false, growth_rate = 2)
+    constructor(level, color='rgb(0,0,255)', position = {x:1000,y:200}, radius = 80, thickness = 2, is_growing = false, is_moving = false, growth_rate = 2)
      {
 
         this.level = level; 
@@ -239,7 +239,7 @@ export default class SweetSpotCircle {
     }
 
 
-    /*
+    
     draw(){
         // Save the current context state
         this.ctx.save();
@@ -273,49 +273,8 @@ export default class SweetSpotCircle {
         // Restore the context to its original state
         this.ctx.restore();
     }
-    */
+    
 
-    draw(){
-        // Save the current context state
-        this.ctx.save();
-    
-        // For thin opaque outer oval
-        this.ctx.globalAlpha = 0.05;
-        this.ctx.beginPath();
-        // Creating an oval instead of a circle
-       // this.ctx.ellipse(this.position.x, this.position.y, this.radius * 0.3, this.radius, 0, 0, Math.PI * 2);
-        this.ctx.ellipse(this.position.x, this.position.y, this.radius * 0.4, this.radius * 1.6, 0, 0, Math.PI * 2);
-
-
-        this.ctx.strokeStyle = this.color;
-        this.ctx.shadowColor = this.color;
-        this.ctx.shadowBlur = this.puffy ? 5 : 40;
-        this.ctx.lineWidth = this.puffy ? this.thickness*2 : this.thickness;
-        this.ctx.stroke();
-        this.ctx.closePath();
-    
-        // Restore the context to its original state
-        this.ctx.restore();
-    
-        // Save the context state again before drawing inner oval
-        this.ctx.save();
-    
-        // For thick transparent inner oval
-        this.ctx.globalAlpha = 0.05;
-        this.ctx.beginPath();
-        // Adjust the second radius parameter to make the oval more elongated
-       
-        //this.ctx.ellipse(this.position.x, this.position.y, (this.baseRadius-10) * 0.3, this.baseRadius-10, 0, 0, Math.PI * 2);
-        this.ctx.ellipse(this.position.x, this.position.y, (this.baseRadius - 10) * 0.4, (this.baseRadius - 10) * 1.6, 0, 0, Math.PI * 2);
-
-        this.ctx.strokeStyle = this.color;
-        this.ctx.lineWidth = 9;
-        this.ctx.stroke();
-        this.ctx.closePath();
-    
-        // Restore the context to its original state
-        this.ctx.restore();
-    }
     
 
 
